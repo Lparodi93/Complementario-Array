11d586ab0fbc5bffa3d0101e0751b631b356294d
Lucas Parodi <lucas.parodi93@gmail.com>
2022-02-16 23:39:20 -0300

    primer entrega

--- main.js
+++ main.js
@@ -1,47 +1,58 @@
 let selector = 0;
 let poke;
+let inPoke;
+let pokeName;
+let miPoke;
 
-const kanto = [{ id: "001", name: "Bulbasaur", type: "Planta/Veneno" },
-{ id: "002", name: "Ivysaur", type: "Planta/Veneno" },
-{ id: "003", name: "Venasaur", type: "Planta/Veneno" },
-{ id: "004", name: "Charmander", type: "Fuego" },
-{ id: "005", name: "Charmeleon", type: "Fuego" },
-{ id: "006", name: "Charizard", type: "Fuego/Volador" },
-{ id: "007", name: "Squirtle", type: "Agua" },
-{ id: "008", name: "Wartortle", type: "Agua" },
-{ id: "009", name: "Blastoise", type: "Agua" },
-{ id: "0010", name: "Caterpie", type: "Bicho" },
-{ id: "011", name: "Metapod", type: "Bicho" },
-{ id: "012", name: "Butterfly", type: "Bicho" }
+const kanto = [
+    { id: "1", name: "BULBASAUR", type: "Planta/Veneno", desc: "Este pokemon nace con una semilla en el lomo que crece con el paso del tiempo" },
+    { id: "2", name: "IVYSAUR", type: "Planta/Veneno", desc: "Cuando le crece bastante el bulbo del lomo, pierde la capacidad de erguirse sobre las patas traseras" },
+    { id: "3", name: "VENASAUR", type: "Planta/Veneno", desc: "La planta florece cuando absorbe energía solar, lo cual le obliga a buscar siempre la luz del sol" },
+    { id: "4", name: "CHARMANDER", type: "Fuego", desc: "Prefiere las cosas calientes. Dicen que cuando llueve le sale vapor de la punta de la cola" },
+    { id: "5", name: "CHARMELEON", type: "Fuego", desc: "Este Pokémon de naturaleza agresiva ataca en combate con su cola llameante y hace trizas al rival con sus afiladas garras" },
+    { id: "6", name: "CHARIZARD", type: "Fuego/Volador", desc: "Escupe un fuego tan caliente que funde las rocas. Causa incendios forestales sin querer." },
+    { id: "7", name: "SQUIRTLE", type: "Agua", desc: "Cuando retrae su largo cuello en el caparazón, dispara agua a una presión increíble" },
+    { id: "8", name: "WARTORTLE", type: "Agua", desc: "Se lo considera un símbolo de longevidad. Los ejemplares más ancianos tienen musgo sobre el caparazón" },
+    { id: "9", name: "BLASTOISE", type: "Agua", desc: "Para acabar con su enemigo, lo aplasta con el peso de su cuerpo. En momentos de apuro, se esconde en el caparazón" },
+    { id: "10", name: "CATERPIE", type: "Bicho", desc: "Para protegerse, despide un hedor horrible por las antenas con el que repele a sus enemigos" },
+    { id: "11", name: "METAPOD", type: "Bicho", desc: "Como en este estado solo puede endurecer su coraza, permanece inmóvil a la espera de evolucionar" },
+    { id: "12", name: "BUTTERFLY", type: "Bicho", desc: "Aletea a gran velocidad para lanzar al aire sus escamas extremadamente tóxicas" }
 ];
 
-
+alert("Bienvenidos a la Pokedex");
 init();
 
+//----------------------------------------------FUNCIONES---------------------------------------------->
 function init() {
+    selector = parseInt(prompt("ingrese 1 para buscar Pokémon por nombre o 2 para buscar por número. 3 para ver referencia"));
+    pokeBusqueda();
+}
 
-    selector = parseInt(prompt("ingrese 1 para ver todos o 2 para elegir un 'Número'"));
-
+function pokeBusqueda() {
     if (selector === 1) {
-
-        for (const pokemon of kanto) {
-            console.log(pokemon.id, pokemon.name, pokemon.type);
+        do {
+            inPoke = prompt("Ingresa nombre de Pokémon");
+            miPoke = kanto.find(item => item.name.toLocaleUpperCase() === inPoke.toLocaleUpperCase());
+            pokeName = miPoke.name + " " + "El pokemón tipo" + " " + miPoke.type + ":" + " " + miPoke.desc;
+            console.log(pokeName);
         }
+        while (!miPoke)
 
     } else if (selector === 2) {
-
         do {
-            poke = parseInt(prompt("Ingrese número de Pokémon"))
-        } while (isNaN(poke));
-
-        if (poke > 12) {
-            poke = 1;
+            inPoke = prompt("Ingrese número de Pokémon");
+            miPoke = kanto.find(item => item.id === inPoke);
+            pokeName = miPoke.name + " " + "El pokemón tipo" + " " + miPoke.type + ":" + " " + miPoke.desc;
+            console.log(pokeName);
         }
-        
-        console.log(kanto[poke - 1])
+        while (!miPoke);
+
+    } else if (selector === 3) {
+        for (const pokemon of kanto) {
+            document.write(pokemon.id + " " + pokemon.name + "<br>");
+        }
 
     } else {
-
         alert("El dato no es válido")
         init();
     }
